6b756ed4c47fb9063db233e5e16f5651a8596b23
戴鹏云 <coolswitch@qq.com>
2020-09-06 23:22:24 +0800

    引入element table

--- src/components/ViewData.vue
+++ src/components/ViewData.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="data-warp">
-    <table>
-      <header>
-        <th>
-          <td>xxx</td>
-          <td>xxx</td>
-          <td>xxx</td>
-          <td>xxx</td>
-          <td>xxx</td>
-        </th>
-      </header>
-      <tbody>
-        <tr>
-          <td>xxx</td>
-          <td>xxx</td>
-          <td>xxx</td>
-          <td>xxx</td>
-          <td>xxx</td>
-        </tr>
-      </tbody>
-    </table>
+    <el-table
+      size="mini"
+      height="100%"
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -28,10 +28,35 @@ export default {
   name: 'ViewData',
   props: {
     msg: String
+  },
+  data () {
+    return {
+
+          tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }]
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.data-warp {
+    height: 100%;
+    overflow: hidden;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './pages/App.vue'
 import store from './store';
+import { Table, TableColumn } from 'element-ui';
 
 Vue.config.productionTip = false
+Vue.component(Table.name, Table);
+Vue.component(TableColumn.name, TableColumn);
 
 new Vue({
   store,
